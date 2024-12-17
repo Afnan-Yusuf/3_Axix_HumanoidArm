a0944a9db4c5a7603b39bf79d3f1672d1c8101e4
Afnan-Yusuf <afnanyusufpp@gmail.com>
2024-12-17 13:07:21 +0530

    notworking

--- include/MotorInit.h
+++ include/MotorInit.h
@@ -1,35 +1,70 @@
 #pragma once
 #include <AccelStepper.h>
 
-#define LSx_direction_pin 13
-#define LSx_step_pin 12
-#define RSx_direction_pin 11
-#define RSx_step_pin 10
+#define LSa_DirPin 16
+#define LSa_StepPin 4
+#define LSb_DirPin 2
+#define LSb_StepPin 15
+#define motorInterfaceType 1
+#define enablepin 17
 
-#define LSy_direction_pin 14
-#define LSy_step_pin 15
-#define RSy_direction_pin 18
-#define RSy_step_pin 19
+long left_pitchpos = 0;
+long left_rollpos = 0;
 
+// Create a new instance of the AccelStepper class:
+AccelStepper LSx = AccelStepper(motorInterfaceType, LSa_StepPin, LSa_DirPin);
+AccelStepper LSy = AccelStepper(motorInterfaceType, LSb_StepPin, LSb_DirPin);
 
-#define ENA_pin 9
+void ArmInit() {
+  // Set the maximum speed and acceleration:
+  LSx.setMaxSpeed(100);
+  LSx.setAcceleration(500);
 
-const int stepsPerRevolution = 200;
-
-AccelStepper LSx(1, LSx_step_pin, LSx_direction_pin);
-AccelStepper RSx(1, RSx_step_pin, RSx_direction_pin);
-AccelStepper LSy(1, LSy_step_pin, LSy_direction_pin);
-AccelStepper RSy(1, RSy_step_pin, RSy_direction_pin);
-
-
-void ArmInit()
-{
-    LSx.setMaxSpeed(1000);
-    RSx.setMaxSpeed(1000);
-    LSy.setMaxSpeed(1000);
-    RSy.setMaxSpeed(1000);
-    pinMode(ENA_pin, OUTPUT);
-    digitalWrite(ENA_pin, HIGH);
+  LSy.setMaxSpeed(100);
+  LSy.setAcceleration(500);
+  pinMode(enablepin, OUTPUT);
+  digitalWrite(enablepin, LOW);
 }
 
 
+void testmot() {
+  LSy.move(8000);
+  LSx.move(8000);
+}
+
+
+
+void left_arm_pitch_roll(int pitch_pos, int roll_pos)
+{
+    long cuurent_Y = LSy.currentPosition();
+    long cuurent_X = LSx.currentPosition();
+
+    left_pitchpos = (cuurent_Y - cuurent_X) /2;
+    left_rollpos = (cuurent_X + cuurent_Y) /2;
+
+    long delta_pitch = pitch_pos - left_pitchpos;   
+    long delta_roll = roll_pos - left_rollpos;
+    
+    
+    long LSx_delta_steps = delta_pitch + delta_roll;
+    long LSy_delta_steps = delta_roll - delta_pitch;
+    Serial.print(left_pitchpos);
+    Serial.print("\t");
+    Serial.print(left_rollpos);
+    Serial.print("\t");
+    Serial.print(delta_pitch);
+    Serial.print("\t");
+    Serial.print(delta_roll);
+    Serial.print("\t");
+    Serial.print(LSx_delta_steps);
+    Serial.print("\t");
+    Serial.print(LSy_delta_steps);
+    Serial.print("\t");
+    Serial.print(LSx.currentPosition());
+    Serial.print("\t");
+    Serial.println(LSy.currentPosition());
+
+    LSx.moveTo(LSx_delta_steps);
+    LSy.moveTo(LSy_delta_steps);
+    
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
+#include <MotorInit.h>
 
 
 
 void setup() {
-  
+  ArmInit();
+  Serial.begin(115200);
 }
 
 void loop() {
-  
+  left_arm_pitch_roll(100,0);
+  LSx.run();
+  LSy.run();
 }

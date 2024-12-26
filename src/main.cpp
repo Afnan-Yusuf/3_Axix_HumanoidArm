ac9eb421055edc9bb75526e713184d166861c355
Afnan-Yusuf <afnanyusufpp@gmail.com>
2024-12-26 20:02:22 +0530

    working

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,26 @@
 #include <Arduino.h>
 #include <MotorInit.h>
 
-const float SHOULDER_PITCH_AMPLITUDE = 90; // Maximum forward/backward swing
-const float SHOULDER_ROLL_OFFSET = 10.0;   // Base roll position
-const float ELBOW_AMPLITUDE = 25.0;        // Elbow swing amplitude
-const float ELBOW_OFFSET = 95.0;           // Center position for elbow
-const int WALK_CYCLE_MS = 2500;            // Time for one complete cycle
-const float RIGHT_PHASE_OFFSET = 180.0;    // Opposite phase for right arm
+const float SHOULDER_PITCH_AMPLITUDE = 90; 
+const float SHOULDER_ROLL_OFFSET = 10.0;   
+const float ELBOW_AMPLITUDE = 25.0;        
+const float ELBOW_OFFSET = 95.0;           
+const int WALK_CYCLE_MS = 2500;            
+const float RIGHT_PHASE_OFFSET = 180.0;
 
 bool isWalking = false;
 unsigned long walkStartTime = 0;
 char cmd = 's';
 bool walkphase = false;
 
+
+#define RXD2 16
+#define TXD2 17
+
 void setup()
 {
   Serial.begin(115200);
+  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
   ArmInit();
 }
 
@@ -24,6 +29,32 @@ void loop()
   if (Serial.available())
   {
     cmd = Serial.read();
+    switch (cmd)
+    {
+    case 'w': 
+      isWalking = true;
+      walkStartTime = millis();
+      Serial.println("Walking started");
+      break;
+    case 's':
+      isWalking = false;
+      moveWrists(0.0, 0, 0.0, 0); // Left shoulder
+      left_elbow_servo.write(ELBOW_OFFSET);
+      right_elbow_servo.write(ELBOW_OFFSET);
+      Serial.println("Walking stopped");
+      break;
+    case 'd':
+      disablesteppers();
+      break;
+    case 'e':
+      enablesteppers();
+      break;
+    }
+  }
+
+  if (Serial1.available())
+  {
+    cmd = Serial1.read();
     switch (cmd)
     {
     case 'w': 
@@ -59,13 +90,13 @@ void loop()
     {
       if (walkphase == false)
       {
-        moveWrists(SHOULDER_ROLL_OFFSET, 0, SHOULDER_ROLL_OFFSET, 30); // Forward swing
+        moveWrists(SHOULDER_ROLL_OFFSET, 0, -SHOULDER_ROLL_OFFSET, 30); // Forward swing
         left_elbow_servo.write(90);
         walkphase = true;
       }
       else
       {
-        moveWrists(SHOULDER_ROLL_OFFSET, 30, SHOULDER_ROLL_OFFSET, 0); // Backward swing
+        moveWrists(SHOULDER_ROLL_OFFSET, 30, -SHOULDER_ROLL_OFFSET, 0); // Backward swing
         left_elbow_servo.write(120);
         walkphase = false;
       }
